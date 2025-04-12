65e47e48c0f62a8b4cd7d609547978f561ae4779
Sienna Baird <siennabaird21@gmail.com>
2025-04-12 10:41:49 -0700

    Initial Commit

---
+++ GitPractice/GitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPracticeApp.swift
+//  GitPractice
+//
+//  Created by Sienna Tanner on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct GitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
